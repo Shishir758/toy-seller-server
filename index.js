43c9507cb95ce5c6be6568f3a2101e21487e2cd3
Shishir758 <studentshishir@gmail.com>
2023-05-20 01:40:50 +0600

    deletedOne route created

--- index.js
+++ index.js
@@ -92,6 +92,15 @@ async function run() {
     });
   
 
+      //deletedOne route created
+      app.delete('/products/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) }
+        const result = await myColl.deleteOne(query);
+        res.send(result)
+      })
+  
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
